09773ab957ff0b3a33422131a881658abac9f1ee
DRuedaM <davidruedamartin26@gmail.com>
2024-01-24 20:26:50 +0100

    Update Main.java
    Se agrega el funcionamiento basico para realizar una peticion a la api de animelist

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class Main
 {
-	 public static void main(String[] args)
+	public static void main(String[] args)
 	{
-		System.out.println("prueba");
+		try 
+		{
+			HttpRequest request = HttpRequest.newBuilder()
+					.uri(URI.create("https://myanimelist.p.rapidapi.com/anime/52991"))
+					.header("X-RapidAPI-Key", "8753c6cf24msh53b46ad39a8d287p1eb4f4jsn6e87232224ca")
+					.header("X-RapidAPI-Host", "myanimelist.p.rapidapi.com")
+					.method("GET", HttpRequest.BodyPublishers.noBody())
+					.build();
+			HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+			System.out.println(response.body());
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		
 	}
 }
